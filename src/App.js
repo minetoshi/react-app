d8b81f4fa9d761e878775f50d7181154de7e6b19
Tomoya Minetoshi <minetoshi.api@gmail.com>
2020-11-22 18:26:11 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import React, {Component} from 'react';
 
 function App() {
   return (
-    <div><h1>Hello, world!</h1></div>
+    <h1>Hello, world!</h1>
   );
 }
+
+
+// function App() {
+//   return React.createElement(
+//     "div",
+//     null,
+//     "hello,world"
+//   );
+// }
+
 
 export default App;
