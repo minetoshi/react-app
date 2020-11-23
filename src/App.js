b74e665308e9c99428eb95ecbd8784ac0edbfcee
Tomoya Minetoshi <minetoshi.api@gmail.com>
2020-11-23 11:49:30 +0900

    create class component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
 
-class App extends Component{
-  render(){
-    // const greeting = "Hi! tom"
-    // const dom = <h1 className= "foo">{greeting}</h1>;
-    // return dom;
+// class App extends Component{
+//   render(){
+//     // const greeting = "Hi! tom"
+//     // const dom = <h1 className= "foo">{greeting}</h1>;
+//     // return dom;
 
-    // return <input type="text" onClick={() => {console.log("I am clicked")}}/>
-    // return <input type="text" onChange={() => {console.log("I am clicked")}}/>
+//     // return <input type="text" onClick={() => {console.log("I am clicked")}}/>
+//     // return <input type="text" onChange={() => {console.log("I am clicked")}}/>
 
-    return(
-<React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onClick={() => {console.log("I am clicked")}}/>
-        </React.Fragment>
-    )
+//     return(
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onClick={() => {console.log("I am clicked")}}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
 
-  }
+const App = () => {
+  return <div>
+    <Cat />
+    <Cat />
+    
+    </div>
+}
+
+const Cat = () => {
+  return <div>meow!</div>
 }
 
 export default App;
