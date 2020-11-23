38b6f519b238bd422cd010e02eee7da893e66303
Tomoya Minetoshi <minetoshi.api@gmail.com>
2020-11-23 11:35:40 +0900

    more jsx

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, {Component} from 'react';
 
-function App() {
-  return (
-    <h1>Hello, world!</h1>
-  );
+class App extends Component{
+  render(){
+    // const greeting = "Hi! tom"
+    // const dom = <h1 className= "foo">{greeting}</h1>;
+    // return dom;
+
+    // return <input type="text" onClick={() => {console.log("I am clicked")}}/>
+    // return <input type="text" onChange={() => {console.log("I am clicked")}}/>
+
+    return(
+<React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onClick={() => {console.log("I am clicked")}}/>
+        </React.Fragment>
+    )
+
+  }
 }
-
-
-// function App() {
-//   return React.createElement(
-//     "div",
-//     null,
-//     "hello,world"
-//   );
-// }
-
 
 export default App;
