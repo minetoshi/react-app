c2ae3cb8e95243688f388d60c33f53d9347bb4c5
Tomoya Minetoshi <minetoshi.api@gmail.com>
2020-11-28 08:23:43 +0900

    commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@
 import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENTS = 'CREATE_EVENTS'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -13,4 +14,9 @@ export const readEvents = () => async dispatch => {
     const response = await axios.get('https://udemy-utils.herokuapp.com/api/v1/events?token=token123')
     console.log(response)
     dispatch({ type: 'READ_EVENTS',response})
+}
+
+export const postEvent = values => async dispatch => {
+    const response = await axios.post('https://udemy-utils.herokuapp.com/api/v1/events?token=token123',values)
+    dispatch({ type: 'CREATE_EVENTS',response})
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux' //reducerを結合させるやつ
+import { reducer as form } from 'redux-form'
 import events from './events'
 
-export default combineReducers({ events })
+export default combineReducers({ events, form })
 //export default combineReducers({ foo, bar, baz }) //複数あったらこんな感じ
 
